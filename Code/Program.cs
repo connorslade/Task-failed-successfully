8f859a6ba00904aa0009df775f5b3ac301918435
Connor Slade <connor@connorcode.com>
2021-01-19 22:19:22 -0500

    Add Code

---
+++ Code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinXP2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// Created by Sigma76 AKA Basicprogrammer10
+        /// Github: https://github.com/Basicprogrammer10/Task-failed-successfully
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            MessageBox.Show("Task failed successfully", "Windows XP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
